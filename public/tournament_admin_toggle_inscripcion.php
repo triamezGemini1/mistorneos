e740af698a1b2b23f6c1b7a94b562415129eb8af
Trino Amezquita <laestaciondeldomino@gmail.com>
2026-03-11 12:40:59 -0400

    Resolviendo error de conexion en inscripcion en sitio 3

--- public/tournament_admin_toggle_inscripcion.php
+++ public/tournament_admin_toggle_inscripcion.php
@@ -1,10 +1,11 @@
-﻿<?php
+<?php
 /**
  * Endpoint público para inscribir/desinscribir jugadores
  * Actúa como proxy para api/tournament_admin_toggle_inscripcion.php
  * para evitar el bloqueo de .htaccess
+ * Iniciar sesión con el mismo mecanismo que index.php para que la cookie se reconozca.
  */
-
+require_once __DIR__ . '/../config/session_start_early.php';
 require_once __DIR__ . '/../api/tournament_admin_toggle_inscripcion.php';
 
 
